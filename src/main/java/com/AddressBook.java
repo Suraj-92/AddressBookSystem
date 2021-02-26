fdd1805fe3881698714a4700528ec28c8319fc53
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-26 12:40:24 +0530

    UC-1 Read Person Done

---
+++ src/main/java/com/AddressBook.java
@@ -0,0 +1,22 @@
+package com;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        String firstName,lastName,address,city,state,zipCode,phoneNumber;
+        System.out.println("Welcome To Address Book");
+        System.out.println("Enter First Name");
+        firstName=GetData.getStringValue();
+        System.out.println("Enter Last Name");
+        lastName=GetData.getStringValue();
+        System.out.println("Enter Address");
+        address=GetData.getStringValue();
+        System.out.println("Enter City");
+        city=GetData.getStringValue();
+        System.out.println("Enter State");
+        state=GetData.getStringValue();
+        System.out.println("Enter Zip");
+        zipCode=GetData.getStringValue();
+        System.out.println("Enter phone number");
+        phoneNumber=GetData.getStringValue();
+    }
+}
