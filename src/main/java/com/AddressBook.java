45e1349dc89c9cd365d3695f1b7ac31f01a63385
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-27 19:54:18 +0530

    Search and view By city Or state

--- src/main/java/com/AddressBook.java
+++ src/main/java/com/AddressBook.java
@@ -1,9 +1,11 @@
 package com;
 
-public class AddressBook { public static void main(String[] args) throws AddressBookException {
-    int i = 0;
+public class AddressBook {
+    public static void main(String[] args) {
+    int choice,i=0;
     final Menu menu = new Menu();
-    while (i == 0) {
+    while(i==0)
+    {
         System.out.println("--- Address Book Management ---\n");
         System.out.println("\t--MENU--");
         System.out.println("1: Add New Person      ");
@@ -11,33 +13,39 @@ public class AddressBook { public static void main(String[] args) throws Address
         System.out.println("3: Edit Person     ");
         System.out.println("4: Delete Person     ");
         System.out.println("5: Sort     ");
-        System.out.println("6: Search     ");
-        System.out.println("7: Exit		       \n");
+        System.out.println("6: View Person By City and State     ");
+        System.out.println("7: Search");
+        System.out.println("8: Exit		       \n");
+//			System.out.println(" -----------------------");
         System.out.println("--- Enter Your Choice ---");
-        int choice = GetData.getIntValue();
-        switch (choice) {
-            case 1:
+        choice = GetData.getIntValue();
+        switch(choice)
+        {
+            case 1 :
                 menu.addPerson();
                 break;
-            case 2:
+            case 2 :
                 menu.display();
                 break;
-            case 3:
+            case 3 :
                 menu.editPerson();
                 break;
-            case 4:
-                menu.delete();
+            case 4 :
+                menu.deleteRecord();
                 break;
-            case 5:
+            case 5 :
                 menu.sortRecords();
                 break;
-            case 6:
-                menu.searchInRecords();
+            case 6 :
+                menu.viewByCityAndState();
                 break;
-            case 7:
-                i = 1;
+            case 7 :
+                menu.searchByCityState();
                 break;
-            default:
+            case 8 :
+                i=1;
+                break;
+            default :
                 System.out.println("Please Enter Valid Option!!!");
         }
     }
--- src/main/java/com/Menu.java
+++ src/main/java/com/Menu.java
@@ -1,84 +1,27 @@
 package com;
-import java.util.*;
 
-public class Menu implements IMenu {
-    List<Person> personList = new ArrayList<>();
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 
-    public static void searchByCity(List<Person> person) {
-        String search;
-        List<Person> matches = new ArrayList<>();
-        System.out.println("Enter First Name to search : ");
-        search = GetData.getStringValue();
-        int flag = 0;
-        for (Person p : person) {
-            if (p.getCity().equalsIgnoreCase(search)) {
-                flag = 1;
-                matches.add(p);
-            }
-        }
-        if (flag == 1) {
-            System.out.println("...Match Found...");
-            for (Person p : matches) {
-                System.out.println(p);
-            }
-        } else {
-            System.out.println("Match Not Found!!!");
-        }
-    }
+public class Menu {
+    List<Person> PERSON = new ArrayList<Person>();
 
-    public static void searchByState(List<Person> person) {
-        String search;
-        int flag = 0;
-        List<Person> matches = new ArrayList<>();
-        System.out.println("Enter First Name to search : ");
-        search = GetData.getStringValue();
-        for (Person p : person) {
-            if (p.getState().equalsIgnoreCase(search)) {
-                flag = 1;
-                matches.add(p);
-            }
-        }
-        if (flag == 1) {
-            System.out.println("...Match Found...");
-            for (Person p : matches) {
-                System.out.println(p);
-            }
-        } else {
-            System.out.println("Match Not Found!!!");
-        }
-    }
-
-    public static void sortByName(List<Person> person) {
-        person.sort(Person.firstNameSorting);
-        person.forEach(System.out::println);
-    }
-
-    public static void sortByCity(List<Person> person) {
-        person.sort(Person.citySorting);
-        person.forEach(System.out::println);
-    }
-
-    public static void sortByState(List<Person> person) {
-        person.sort(Person.stateSorting);
-        person.forEach(System.out::println);
-    }
-
-    public static void sortByZipCode(List<Person> person) {
-        person.sort(Person.zipSorting);
-        person.forEach(System.out::println);
-    }
-
-    public void addPerson() {
-        int i = 0;
+    public void addPerson()
+    {
+        int i=0;
         String firstName = null;
-        final String lastName, address, city, state, phoneNumber, zipCode;
-        while (i == 0) {
+        final String lastName, address, city, state, phoneNumber,zipCode;
+        while(i==0) {
             System.out.print("Enter First Name : ");
             firstName = GetData.getStringValue();
-            if (checkExists(firstName)) {
+            if (checkExists(firstName))
+            {
                 System.out.println("Person Name Already Exists!!\nPlease enter different name...");
-            } else {
-                i = 1;
+            }
+            else {
+                i=1;
             }
         }
         System.out.print("Enter Last Name : ");
@@ -94,94 +37,89 @@ public class Menu implements IMenu {
         System.out.print("Enter state : ");
         state = GetData.getStringValue();
 
-        personList.add(new Person(firstName, lastName, address, city, state, phoneNumber, zipCode));
+        PERSON.add(new Person(firstName,lastName,address,city,state,phoneNumber,zipCode));
     }
 
-    public void display() {
-        if (personList.isEmpty()) {
-            System.out.println("No Records To Display!!!");
-        } else {
-            personList.forEach(System.out::println);
+    public void display()
+    {
+        if (PERSON.isEmpty())
+        {
+            System.out.println("No Records!!!");
         }
-
-    }
-
-    public void editPerson() throws AddressBookException {
-        int id, i = 0;
-        String address, city, state, phoneNumber, zipCode;
-        try {
-            if (personList.isEmpty()) {
-                System.out.println("No Records To Edit!!!");
-            } else {
-                for (Person person : personList) {
-                    System.out.println("ID: #" + personList.indexOf(person) + " : " + person);
-                }
-                System.out.print("\nEnter #ID to Edit Contact : ");
-                id = GetData.getIntValue();
-                System.out.println(personList.get(id));
-                while (i == 0) {
-                    System.out.println("What You Want to edit...\n"
-                            + "\t1: Address\n"
-                            + "\t2: city\n"
-                            + "\t3: State\n"
-                            + "\t4: Phone\n"
-                            + "\t5: Zip Code\n"
-                            + "\t6. Save And Exit\n");
-                    int choice = GetData.getIntValue();
-                    switch (choice) {
-                        case 1:
-                            System.out.print("Enter new Address : ");
-                            address = GetData.getStringValue();
-                            personList.get(id).setAddress(address);
-                            break;
-                        case 2:
-                            System.out.print("Enter new City : ");
-                            city = GetData.getStringValue();
-                            personList.get(id).setCity(city);
-                            break;
-                        case 3:
-                            System.out.print("Enter new State : ");
-                            state = GetData.getStringValue();
-                            personList.get(id).setState(state);
-                            break;
-                        case 4:
-                            System.out.print("Enter new Phone : ");
-                            phoneNumber = GetData.getStringValue();
-                            personList.get(id).setPhone(phoneNumber);
-                            break;
-                        case 5:
-                            System.out.print("Enter new Zip Code : ");
-                            zipCode = GetData.getStringValue();
-                            personList.get(id).setZip(zipCode);
-                            break;
-                        case 6:
-                            i = 1;
-                            break;
-                        default:
-                            System.out.println("Please Enter Valid Option");
-                    }
-                    System.out.println(personList.get(id));
-                }
+        else {
+            for (Person person : PERSON) {
+                System.out.println(person);
             }
-        } catch (IndexOutOfBoundsException e) {
-            throw new AddressBookException("Entered Wrong #ID", AddressBookException.exceptionType.ENTERED_WRONG_ID);
+        }
+
+    }
+
+    public void editPerson()
+    {
+        int id,choice = 0, i=0;
+        String firstName,lastName,address,city,state,phoneNumber,zipCode;
+        for(Person person: PERSON)
+        {
+            System.out.println("ID: #"+PERSON.indexOf(person)+" : "+person);
+        }
+        System.out.print("\nEnter #ID to Edit Contact : ");
+        id = GetData.getIntValue();
+        System.out.println(PERSON.get(id));
+        while(i==0) {
+            System.out.println("What You Want to edit...\n"
+                    + "\t1: Address\n"
+                    + "\t2: city\n"
+                    + "\t3: State\n"
+                    + "\t4: Phone\n"
+                    + "\t5: Zip Code\n"
+                    + "\t6. Save And Exit\n");
+            choice = GetData.getIntValue();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter new Address : ");
+                    address = GetData.getStringValue();
+                    PERSON.get(id).setAddress(address);
+                    break;
+                case 2:
+                    System.out.print("Enter new City : ");
+                    city = GetData.getStringValue();
+                    PERSON.get(id).setCity(city);
+                    break;
+                case 3:
+                    System.out.print("Enter new State : ");
+                    state = GetData.getStringValue();
+                    PERSON.get(id).setState(state);
+                    break;
+                case 4:
+                    System.out.print("Enter new Phone : ");
+                    phoneNumber = GetData.getStringValue();
+                    PERSON.get(id).setPhoneNumber(phoneNumber);
+                    break;
+                case 5:
+                    System.out.print("Enter new Zip Code : ");
+                    zipCode = GetData.getStringValue();
+                    PERSON.get(id).setZipCode(zipCode);
+                    break;
+                case 6:
+                    i=1;
+                    break;
+                default:
+                    System.out.println("Please Enter Valid Option");
+            }
+            System.out.println(PERSON.get(id));
         }
     }
 
-    public void delete() throws AddressBookException {
-        try {
-            int id;
-            if (personList.isEmpty()) {
-                System.out.println("No Records To Delete!!!");
-            } else {
-                personList.stream().map(p -> "ID: #" + personList.indexOf(p) + " : " + p).forEach(System.out::println);
-                System.out.print("\nEnter #ID to delete Contact : ");
-                id = GetData.getIntValue();
-                personList.remove(id);
-            }
-        } catch (IndexOutOfBoundsException e) {
-            throw new AddressBookException("Entered Wrong #ID", AddressBookException.exceptionType.ENTERED_WRONG_ID);
+    public void deleteRecord()
+    {
+        int id;
+        for(Person p: PERSON)
+        {
+            System.out.println("ID: #"+PERSON.indexOf(p)+" : "+p);
         }
+        System.out.print("\nEnter #ID to delete Contact : ");
+        id = GetData.getIntValue();
+        PERSON.remove(id);
     }
 
     public void sortRecords() {
@@ -194,16 +132,16 @@ public class Menu implements IMenu {
         int choice = GetData.getIntValue();
         switch (choice) {
             case 1:
-                sortByName(personList);
+                Sort.sortByName(PERSON);
                 break;
             case 2:
-                sortByCity(personList);
+                Sort.sortByZip(PERSON);
                 break;
             case 3:
-                sortByState(personList);
+                Sort.sortByState(PERSON);
                 break;
             case 4:
-                sortByZipCode(personList);
+                Sort.sortByZip(PERSON);
                 break;
             case 5:
                 return;
@@ -212,32 +150,75 @@ public class Menu implements IMenu {
         }
     }
 
-    public boolean checkExists(String firstName) {
-        int flag = personList.stream().anyMatch(p -> p.getFirstName().equalsIgnoreCase(firstName)) ? 1 : 0;
-        return flag == 1;
-    }
-
-    public void searchInRecords() {
-        int i = 0;
-        while (i == 0) {
-            System.out.println("1. Search By City\n" +
-                    "2. Search By State\n" +
-                    "3. Back\n" +
-                    "Choose Your Option");
-            int choice = GetData.getIntValue();
-            switch (choice) {
-                case 1:
-                    searchByCity(personList);
-                    break;
-                case 2:
-                    searchByState(personList);
-                    break;
-                case 3:
-                    i = 1;
-                    break;
-                default:
-                    System.out.println("Please Enter Correct Option...");
-            }
+    public void searchByCityState()
+    {
+        int choice=0;
+        System.out.println("Search By\n" +
+                "1: City\n" +
+                "2: State\n" +
+                "3: back");
+        choice= GetData.getIntValue();
+        switch (choice)
+        {
+            case 1 :
+                Search.searchByCity(PERSON);
+                break;
+            case 2 :
+                break;
+            case 3 :
+                return;
+            default:
+                System.out.println("Enter Valid Option");
         }
     }
+
+    public void viewByCityAndState()
+    {
+        Dictionary<String ,String> cityDict = createCityDict();
+        Dictionary<String ,String> stateDict = createStateDict();
+        final String city,state;
+        System.out.println("Enter City");
+        city= GetData.getStringValue();
+        System.out.println("Enter State");
+        state= GetData.getStringValue();
+        Search.searchByCityAndState(cityDict,stateDict);
+    }
+    public Dictionary<String,String> createCityDict()
+    {
+        Dictionary<String,String> cityDict = new Hashtable<String ,String>();
+        for (Person person:PERSON)
+        {
+            cityDict.put(person.getFirstName(),person.getCity());
+        }
+        return cityDict;
+    }
+
+    public Dictionary<String,String> createStateDict()
+    {
+        Dictionary<String,String> stateDict = new Hashtable<String ,String>();
+        for (Person person:PERSON)
+        {
+            stateDict.put(person.getFirstName(),person.getState());
+        }
+        return stateDict;
+    }
+
+    public boolean checkExists(String firstName)
+    {
+        int flag=0;
+        for (Person p: PERSON)
+        {
+            if (p.getFirstName().equals(firstName))
+            {
+                flag=1;
+                break;
+            }
+        }
+        if (flag==1)
+        {
+            return true;
+        }
+        return false;
+    }
 }
+
